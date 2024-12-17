be42b213a05681a7ec62380e7218c7a4bff7d798
michael.g.russell <michael.g.russell@baesystems.us>
2024-12-17 13:12:30 -0700

    Added qr code

---
+++ make_qr_code.py
@@ -0,0 +1,13 @@
+# https://pypi.org/project/qrcode/
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=20,
+    border=1,
+)
+qr.add_data("https://russell-family-christmas.github.io/russell-family-christmas/")
+qr.make(fit=True)
+img = qr.make_image(fill_color="green", back_color="white")
+img.save("site-qr-code.png")
